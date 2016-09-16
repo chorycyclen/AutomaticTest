b11275556de9302816fae684d68aa83db50cb129
chorycyclen <837969204@qq.com>
2016-09-16 20:55:06 +0800

    测试动作更改：用例结束清除Cookies，Suite结束关闭浏览器

--- src/test/java/com/cook/selenium/DriverFactory.java
+++ src/test/java/com/cook/selenium/DriverFactory.java
@@ -3,10 +3,17 @@ package com.cook.selenium;
 
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class DriverFactory {
 
+    private static List<WebDriverThread> webDriverThreadPool =
+            Collections.synchronizedList(new ArrayList<WebDriverThread>());
     private static ThreadLocal<WebDriverThread> driverThread;
 
     @BeforeSuite
@@ -14,7 +21,9 @@ public class DriverFactory {
         driverThread = new ThreadLocal<WebDriverThread>() {
             @Override
             protected WebDriverThread initialValue() {
-                return new WebDriverThread();
+                WebDriverThread webDriverThread = new WebDriverThread();
+                webDriverThreadPool.add(webDriverThread);
+                return webDriverThread;
             }
         };
     }
@@ -24,8 +33,16 @@ public class DriverFactory {
     }
 
     @AfterMethod
-    public static void quitDriver() throws Exception {
-        driverThread.get().quitDriver();
+    public static void clearCookies() throws Exception {
+        getDriver().manage().deleteAllCookies();
+    }
+
+    @AfterSuite
+    public static void closeDrivers() {
+        for (WebDriverThread webDriverThread :
+                webDriverThreadPool) {
+            webDriverThread.quitDriver();
+        }
     }
 
 }
